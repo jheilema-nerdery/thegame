dcaf0da484057c2739b0e959e480b2a24b9c7c8e
Jen Heilemann <jheilema@nerdery.com>
2016-07-03 16:38:20 -0400

    Adding used/action to the db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703183457) do
+ActiveRecord::Schema.define(version: 20160703184943) do
 
   create_table "items", force: :cascade do |t|
     t.string   "case"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20160703183457) do
     t.string   "api_id"
     t.integer  "rarity"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "used",        default: false
+    t.string   "action"
   end
 
 end
