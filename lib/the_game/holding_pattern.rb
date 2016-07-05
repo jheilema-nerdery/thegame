7f705961b579871ec92c89e9047a0a15af6ee668
Jen Heilemann <jheilema@nerdery.com>
2016-07-05 01:12:14 -0400

    array smarray

--- lib/the_game/holding_pattern.rb
+++ lib/the_game/holding_pattern.rb
@@ -48,7 +48,7 @@ module TheGame
 
         # try to stay in the top 10
         if players.none?{|p| p[:PlayerName] == 'jheilema'}
-          item_types = item_types + ItemLibrary::EFFECT_OVER_TIME - '7777'
+          item_types = item_types + ItemLibrary::EFFECT_OVER_TIME - ['7777']
         end
       end
 
