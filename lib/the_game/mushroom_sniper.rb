48d043776a9670759b4405cef0897db0e5c689f7
Jen Heilemann <jheilema@nerdery.com>
2016-07-06 03:37:55 -0400

    sniping mushrooms

---
+++ lib/the_game/mushroom_sniper.rb
@@ -0,0 +1,43 @@
+module TheGame
+  class MushroomSniper
+    def initialize(logger, api)
+      @logger = logger
+      @api = api
+      @players = []
+      @effects = []
+    end
+
+    def time_to_wait
+      60
+    end
+
+    def choose_item_and_player(current_effects = [], players)
+      @players = players
+
+      player = @players.find {|p| not_me(p) && has_tanooki_suit(p) }
+      return [] if player.nil?
+      @logger.debug "Player '#{player[:PlayerName]}' chosen"
+
+      item = find_item
+      return [] if item.nil?
+      @logger.debug "Sniping Mushroom chosen"
+
+      return item, player[:PlayerName]
+    end
+
+  private
+
+    def find_item
+      Item.unused.oldest.named('Mushroom').first
+    end
+
+    def not_me(p)
+      p[:PlayerName] != 'jheilema'
+    end
+
+    def has_tanooki_suit(p)
+      p[:Effects].include?("Tanooki Suit")
+    end
+
+  end
+end
