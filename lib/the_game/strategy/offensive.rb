091327dfee18ec0576ed5331f8a58b0465dbcae8
Jen Heilemann <jheilema@nerdery.com>
2016-07-10 14:57:42 -0400

    I forgot Offensive and TanookiSniper, oops

--- lib/the_game/strategy/offensive.rb
+++ lib/the_game/strategy/offensive.rb
@@ -1,69 +1,71 @@
 class TheGame
-  class Offensive
-    def initialize(logger, api)
-      @logger = logger
-      @api = api
-      @players = []
-    end
-
-    def use_items?
-      true
-    end
-
-    def try_again_in
-      10
-    end
-
-    def choose_item_and_player(players, jen)
-      @players = players
-
-      player = choose_player
-      return [] if player.nil?
-      @logger.debug "Player '#{player[:PlayerName]}' chosen"
-
-      item = find_item(player)
-      return [] if item.nil?
-      @logger.debug "#{item.name} chosen"
-
-      return item, player[:PlayerName]
-    end
-
-    def choose_player
-      sheildless = @players.find_all{|p| no_sheild(p) }
-      sheildless.find{|p| has_multipliers(p) && !has_hadouken(p) } || sheildless.sample
-    end
-
-    def find_item(player)
-      item_types = ItemLibrary::WEAPON
-
-      if has_multipliers(player) && !has_hadouken(player)
-        item_types = ['Hadouken']
+  module Strategy
+    class Offensive
+      def initialize(logger, api)
+        @logger = logger
+        @api = api
+        @players = []
       end
 
-      if is_me(player)
-        item_types = ItemLibrary::PRICK
+      def use_items?
+        true
       end
 
-      item_types -= player[:Effects]
+      def try_again_in
+        10
+      end
 
-      Item.unused.oldest.where(name: item_types).first
+      def choose_item_and_player(players, jen)
+        @players = players
+
+        player = choose_player
+        return [] if player.nil?
+        @logger.debug "Player '#{player[:PlayerName]}' chosen"
+
+        item = find_item(player)
+        return [] if item.nil?
+        @logger.debug "#{item.name} chosen"
+
+        return item, player[:PlayerName]
+      end
+
+      def choose_player
+        sheildless = @players.find_all{|p| no_sheild(p) }
+        sheildless.find{|p| has_multipliers(p) && !has_hadouken(p) } || sheildless.sample
+      end
+
+      def find_item(player)
+        item_types = ItemLibrary::WEAPON
+
+        if has_multipliers(player) && !has_hadouken(player)
+          item_types = ['Hadouken']
+        end
+
+        if is_me(player)
+          item_types = ItemLibrary::PRICK
+        end
+
+        item_types -= player[:Effects]
+
+        Item.unused.oldest.where(name: item_types).first
+      end
+
+      def has_hadouken(player)
+        player[:Effects].include?('Hadouken')
+      end
+
+      def has_multipliers(player)
+        (player[:Effects] & ItemLibrary::MULTIPLIER).length > 0
+      end
+
+      def is_me(p)
+        p[:PlayerName] == 'jheilema'
+      end
+
+      def no_sheild(p)
+        (p[:Effects] & TheGame::ItemLibrary::PROTECTION).empty?
+      end
+
     end
-
-    def has_hadouken(player)
-      player[:Effects].include?('Hadouken')
-    end
-
-    def has_multipliers(player)
-      (player[:Effects] & ItemLibrary::MULTIPLIER).length > 0
-    end
-
-    def is_me(p)
-      p[:PlayerName] == 'jheilema'
-    end
-
-    def no_sheild(p)
-      (p[:Effects] & TheGame::ItemLibrary::PROTECTION).empty?
-    end
-
   end
 end
--- lib/the_game/strategy/tanooki_sniper.rb
+++ lib/the_game/strategy/tanooki_sniper.rb
@@ -1,47 +1,49 @@
 class TheGame
-  class TanookiSniper
-    def initialize(logger, api)
-      @logger = logger
-      @api = api
-      @players = []
-      @effects = []
+  module Strategy
+    class TanookiSniper
+      def initialize(logger, api)
+        @logger = logger
+        @api = api
+        @players = []
+        @effects = []
+      end
+
+      def try_again_in
+        10
+      end
+
+      def use_items?
+        true
+      end
+
+      def choose_item_and_player(players, jen)
+        @players = players
+
+        player = @players.find {|p| not_me(p) && has_tanooki_suit(p) }
+        return [] if player.nil?
+        @logger.debug "Player '#{player[:PlayerName]}' chosen"
+
+        item = find_item
+        return [] if item.nil?
+        @logger.debug "Sniping #{item.name} chosen"
+
+        return item, player[:PlayerName]
+      end
+
+    private
+
+      def find_item
+        Item.unused.oldest.where(name: ItemLibrary::PRICK).first
+      end
+
+      def not_me(p)
+        p[:PlayerName] != 'jheilema'
+      end
+
+      def has_tanooki_suit(p)
+        p[:Effects].include?("Tanooki Suit")
+      end
+
     end
-
-    def try_again_in
-      10
-    end
-
-    def use_items?
-      true
-    end
-
-    def choose_item_and_player(players, jen)
-      @players = players
-
-      player = @players.find {|p| not_me(p) && has_tanooki_suit(p) }
-      return [] if player.nil?
-      @logger.debug "Player '#{player[:PlayerName]}' chosen"
-
-      item = find_item
-      return [] if item.nil?
-      @logger.debug "Sniping #{item.name} chosen"
-
-      return item, player[:PlayerName]
-    end
-
-  private
-
-    def find_item
-      Item.unused.oldest.where(name: ItemLibrary::PRICK).first
-    end
-
-    def not_me(p)
-      p[:PlayerName] != 'jheilema'
-    end
-
-    def has_tanooki_suit(p)
-      p[:Effects].include?("Tanooki Suit")
-    end
-
   end
 end
