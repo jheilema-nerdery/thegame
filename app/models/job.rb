0bbdd51114c7f25fe81008bfb04e5e71cad9a19d
Jen Heilemann <jheilema@nerdery.com>
2016-07-09 22:43:45 -0400

    build job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ActiveRecord::Base
   scope :oldest, ->{ order(:updated_at) }
   scope :newest, ->{ order(:updated_at => :desc) }
+
+  def self.build(target, item)
+    create!(target: target, item_name: item)
+  end
 end
