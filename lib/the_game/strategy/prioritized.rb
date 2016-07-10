cb45abd603895060bcd90c8b8b17d01b46cafbc5
Jen Heilemann <jheilema@nerdery.com>
2016-07-10 14:17:19 -0400

    Prioritized and round robin strategies

---
+++ lib/the_game/strategy/prioritized.rb
@@ -0,0 +1,32 @@
+class TheGame
+  module Strategy
+    class Prioritized
+      def initialize(logger, api)
+        @logger = logger
+        @api = api
+        @strategies = []
+      end
+
+      def use_items?
+        true
+      end
+
+      def try_again_in
+        10
+      end
+
+      def add_strategy(*strategy)
+        @strategies.push(*strategy)
+      end
+
+      def choose_item_and_player(players, jen)
+        @strategies.each do |strat|
+          strategy = ("TheGame::Strategy::" + strat).constantize.new(@logger, @api)
+          result = strategy.choose_item_and_player(players, jen)
+          return result unless result.empty? # break on the first one
+        end
+        []
+      end
+    end
+  end
+end
---
+++ lib/the_game/strategy/round_robin.rb
@@ -0,0 +1,48 @@
+class TheGame
+  module Strategy
+    class RoundRobin
+      def initialize(logger, api)
+        @logger = logger
+        @api = api
+        @strategies = []
+        @index = 0
+      end
+
+      def use_items?
+        true
+      end
+
+      def try_again_in
+        10
+      end
+
+      def add_strategy(*strategy)
+        @strategies.push(*strategy)
+      end
+
+      def choose_item_and_player(players, jen)
+        @strategies.each do |i|
+          strategy = ("TheGame::Strategy::" + next_strategy).constantize.new(@logger, @api)
+          result = strategy.choose_item_and_player(players, jen)
+          increment_index
+
+          return result unless result.empty? # break on the first one
+        end
+
+        return []
+      end
+
+      private
+
+      def next_strategy
+        @strategies[@index]
+      end
+
+      def increment_index
+        @index += 1
+        @index = 0 if @index == @strategies.length
+      end
+
+    end
+  end
+end
