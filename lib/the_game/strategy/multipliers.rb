5517c7999b6e1a8a6c4931aa964013da9eab024d
Jen Heilemann <jheilema@nerdery.com>
2016-07-11 02:00:21 -0400

    OH MY GOD:

--- lib/the_game/strategy/multipliers.rb
+++ lib/the_game/strategy/multipliers.rb
@@ -16,7 +16,7 @@ class TheGame
 
       def choose_item_and_player(players, jen, current_player)
         return [] if jen.effects.include? 'Tanooki Suit'
-        return [] if (item_types - jen.effects).length == 0
+        return [] unless (item_types - jen.effects).length > 0
 
         item = find_item(jen.effects)
         return [] if item.nil?
@@ -26,8 +26,7 @@ class TheGame
 
       def find_item(effects)
         items = item_types - effects
-        @logger.debug("possible Multipliers: #{items.to_s}")
-        Item.unused.oldest.where(name: item_types).first
+        Item.unused.oldest.where(name: items).first
       end
 
       def item_types
