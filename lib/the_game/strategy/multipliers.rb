1ccaf6d0b9af79c8341c94ddafd0356c973a8ad9
Jen Heilemann <jheilema@nerdery.com>
2016-07-11 01:57:18 -0400

    oh my gosh

--- lib/the_game/strategy/multipliers.rb
+++ lib/the_game/strategy/multipliers.rb
@@ -14,19 +14,19 @@ class TheGame
         5
       end
 
-      def choose_item_and_player(players, jen, username)
+      def choose_item_and_player(players, jen, current_player)
         return [] if jen.effects.include? 'Tanooki Suit'
-        return [] unless (item_types - jen.effects).length == 0
+        return [] if (item_types - jen.effects).length == 0
 
         item = find_item(jen.effects)
         return [] if item.nil?
 
-        @logger.debug "Multipliers: #{item.name} chosen"
         return [item, 'jheilema']
       end
 
       def find_item(effects)
         items = item_types - effects
+        @logger.debug("possible Multipliers: #{items.to_s}")
         Item.unused.oldest.where(name: item_types).first
       end
 
--- lib/the_game/strategy/varia.rb
+++ lib/the_game/strategy/varia.rb
@@ -14,18 +14,19 @@ class TheGame
         5
       end
 
-      def choose_item_and_player(players, jen, username)
+      def choose_item_and_player(players, jen, current_player)
         return [] if jen.effects.include? 'Tanooki Suit'
-        return [] unless (item_types - jen.effects).length == 0
+        return [] if (item_types - jen.effects).length == 0
 
-        item = find_item
+        item = find_item(jen.effects)
         return [] if item.nil?
 
         @logger.debug "Suits: #{item.name} chosen"
         return [item, 'jheilema']
       end
 
-      def find_item
+      def find_item(effects)
+        items = item_types - effects
         Item.unused.oldest.where(name: item_types).first
       end
 
