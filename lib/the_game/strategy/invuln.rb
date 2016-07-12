496790a2d80ed154aab8de041bf1e2717f8ae024
Jen Heilemann <jheilema@nerdery.com>
2016-07-12 03:50:04 -0400

    invuln strats

---
+++ lib/the_game/strategy/invuln.rb
@@ -0,0 +1,28 @@
+class TheGame
+  module Strategy
+    class Invuln < BaseStrategy
+      def try_again_in
+        5
+      end
+
+      def choose_item_and_player(players, jen, current_player)
+        return [] if jen.effects.include? 'Tanooki Suit'
+        return [] if (item_types & jen.effects).length > 0
+
+        item = find_item(jen.effects)
+        return [] if item.nil?
+
+        @logger.debug "Suits: #{item.name} chosen"
+        return [item, 'jheilema']
+      end
+
+      def find_item(effects)
+        Item.unused.oldest.where(name: item_types).first
+      end
+
+      def item_types
+        ['Star', 'Gold Ring']
+      end
+    end
+  end
+end
